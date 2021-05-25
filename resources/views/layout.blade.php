850bc9962dd83e16c042f5d36c28126a0504096a
Cesar Acual <augustoacual@gmail.com>
2021-05-25 11:51:21 -0600

    Mostar el detalle de un post

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,26 +1,30 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>Zendero</title>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/framework.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/framework.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/responsive.css">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+
+    @stack('scripts')
 </head>
+
 <body>
     <div class="preload"></div>
     <header class="space-inter">
         <div class="container container-flex space-between">
-            <figure class="logo"><img src="img/logo.png" alt=""></figure>
+            <figure class="logo"><img src="/img/logo.png" alt=""></figure>
             <nav class="custom-wrapper" id="menu">
                 <div class="pure-menu"></div>
                 <ul class="container-flex list-unstyled">
-                    <li><a href="index.html" class="text-uppercase">Home</a></li>
+                    <li><a href="/" class="text-uppercase">Home</a></li>
                     <li><a href="about.html" class="text-uppercase">About</a></li>
                     <li><a href="archive.html" class="text-uppercase">Archive</a></li>
                     <li><a href="contact.html" class="text-uppercase">Contact</a></li>
@@ -34,7 +38,7 @@
     <section class="footer">
         <footer>
             <div class="container">
-                <figure class="logo"><img src="img/logo.png" alt=""></figure>
+                <figure class="logo"><img src="/img/logo.png" alt=""></figure>
                 <nav>
                     <ul class="container-flex space-center list-unstyled">
                         <li><a href="index.html" class="text-uppercase c-white">home</a></li>
@@ -44,9 +48,12 @@
                     </ul>
                 </nav>
                 <div class="divider-2"></div>
-                <p>Nunc placerat dolor at lectus hendrerit dignissim. Ut tortor sem, consectetur nec hendrerit ut, ullamcorper ac odio. Donec viverra ligula at quam tincidunt imperdiet. Nulla mattis tincidunt auctor.</p>
+                <p>Nunc placerat dolor at lectus hendrerit dignissim. Ut tortor sem, consectetur nec hendrerit ut,
+                    ullamcorper ac odio. Donec viverra ligula at quam tincidunt imperdiet. Nulla mattis tincidunt
+                    auctor.</p>
                 <div class="divider-2" style="width: 80%;"></div>
-                <p>© 2017 - Zendero. All Rights Reserved. Designed & Developed by <span class="c-white">Agencia De La Web</span></p>
+                <p>© 2017 - Zendero. All Rights Reserved. Designed & Developed by <span class="c-white">Agencia De La
+                        Web</span></p>
                 <ul class="social-media-footer list-unstyled">
                     <li><a href="#" class="fb"></a></li>
                     <li><a href="#" class="tw"></a></li>
@@ -56,6 +63,7 @@
             </div>
         </footer>
     </section>
-    
+
 </body>
+
 </html>
