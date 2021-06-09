74e45e49161fd46d5a4bd280375956e5b02e4d25
Cesar Acual <augustoacual@gmail.com>
2021-06-09 11:55:11 -0600

    Agregar funcionalidad para agregar a un post video y audio, como contenido enbebido

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -86,6 +86,19 @@
                             <span class="error invalid-feedback">{{ $message }}</span>
                             @enderror
                         </div>
+                         <div class="form-group">
+                            <label for="editor">Contenido embebido (iframe)</label>
+                            <textarea 
+                                name="iframe" 
+                                id="editor" 
+                                class="form-control @error('title') is-invalid @enderror" 
+                                rows="2"
+                                placeholder="Ingresa contenido enbebido (iframe) de audio o video"
+                            >{{ old('iframe', $post->iframe) }}</textarea>
+                            @error('iframe')
+                            <span class="error invalid-feedback">{{ $message }}</span>
+                            @enderror
+                        </div>
                     </div>
                 </div>
             </div>
