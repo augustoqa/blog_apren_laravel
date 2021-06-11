081e3196b7b03834ddb0344f582d8bd002421347
Cesar Acual <augustoacual@gmail.com>
2021-06-11 11:22:31 -0600

    Filtrar posts por categoría

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,11 @@
 @section('content')
 
     <section class="posts container">
-        @foreach($posts as $post)
+    @if (isset($category))
+        <h3>Publicaciones de la categoría {{ $category->name }}</h3>
+    @endif
+
+    @foreach($posts as $post)
         <article class="post">
             @if($post->photos->count() === 1)
             <figure>
@@ -31,7 +35,11 @@
                         <span class="c-gray-1">{{ $post->published_at->format('M d') }}</span>
                     </div>
                     <div class="post-category">
-                        <span class="category text-capitalize">{{ $post->category->name }}</span>
+                        <span class="category text-capitalize">
+                            <a href="{{ route('categories.show', $post->category) }}">
+                                {{ $post->category->name }}
+                            </a>
+                        </span>
                     </div>
                 </header>
                 <h1>{{ $post->title }}</h1>
