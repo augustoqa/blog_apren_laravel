7bee97239a2ecbe385df950553e15ee180c60d89
César Acual <augustoacual@gmail.com>
2021-08-25 17:40:20 -0600

    Agregar permisons a usuarios con laravel-permission

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -166,7 +166,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             <img src="/adminlte/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
 
                             <p>
-                                {{ auth()->user()->name }}
+                                {{ auth()->user()->name }} - {{ auth()->user()->roles->first()->name }}
                                 <small>Desde {{ auth()->user()->created_at->format('d/M/Y') }}</small>
                             </p>
                         </li>
