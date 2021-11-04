eb78a4cfbeb42624b26765dd709ee75b65eac316
César Acual <augustoacual@gmail.com>
2021-11-03 22:05:41 -0600

    Permitir a un usuario ver los Roles en el administrador

---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,125 @@
+@extends('admin.layout')
+
+@section('header')
+<div class="container-fluid">
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1 class="m-0">Roles <small>Listado</small></h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item">
+                    <a href="{{ route('dashboard') }}">
+                        <i class="fas fa-tachometer-alt"></i>
+                        Inicio
+                    </a>
+                </li>
+                <li class="breadcrumb-item active">
+                    <i class="fas fa-list"></i>
+                    Roles
+                </li>
+            </ol>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+</div><!-- /.container-fluid -->
+@endsection
+
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        <h3 class="float-left">Listado de Roles</h3>
+        <a
+            href="{{ route('admin.roles.create') }}"
+            class="btn btn-primary float-right"  
+        >
+            <i class="fas fa-plus"></i>
+            Crear Role
+        </a>
+    </div>
+    <div class="card-body">
+        <table id="roles-table" class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Guard</th>
+                    <th>Roles</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($roles as $role)
+                <tr>
+                    <td>{{ $role->id }}</td>
+                    <td>{{ $role->name }}</td>
+                    <td>{{ $role->guard_name }}</td>
+                    <td>
+                        <a
+                            href="{{ route('admin.roles.show', $role) }}"
+                            class="btn btn-sm btn-default"
+                        >
+                            <i class="fa fa-eye"></i>
+                        </a>
+                        <a href="{{ route('admin.roles.edit', $role) }}" class="btn btn-sm btn-info">
+                            <i class="fa fa-pencil-alt"></i>
+                        </a>
+                        <form
+                            method="POST"
+                            action="{{ route('admin.roles.destroy', $role) }}"
+                            style="display: inline;"
+                        >
+                            {{ csrf_field() }} {{ method_field('DELETE') }}
+                            <button 
+                                class="btn btn-sm btn-danger"
+                                onclick="return confirm('¿Estás seguro de querer eliminar esta Role?')"
+                            >
+                                <i class="fa fa-times"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+
+        </table>
+    </div>
+</div>
+
+@endsection
+
+@push('styles')
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('/adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+@endpush
+
+@push('scripts')
+    <!-- DataTables  & Plugins -->
+    <script src="/adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="/adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="/adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="/adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="/adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="/adminlte/plugins/jszip/jszip.min.js"></script>
+    <script src="/adminlte/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="/adminlte/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="/adminlte/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="/adminlte/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="/adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
+    <script>
+        $(function () {
+            $('#posts-table').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+            });
+        });
+    </script>
+@endpush
