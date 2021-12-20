f875e40a2aa5cf48e8cef2f405c106bfe1689157
César Acual <augustoacual@gmail.com>
2021-12-20 17:14:36 -0600

    Implementar la página de archivo

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
         <h3>{{ $title }}</h3>
     @endif
 
-    @foreach($posts as $post)
+    @forelse($posts as $post)
         <article class="post">
 
             @include( $post->viewType('home') )
@@ -29,7 +29,14 @@
                 </footer>
             </div>
         </article>
-        @endforeach
+    @empty
+
+        <article class="post">
+            <div class="content-post">
+                <h1>No hay publicaciones todavía.</h1>
+            </div>
+        </article>
+    @endforelse
     </section><!-- fin del div.posts.container -->
 
     {{ $posts->links() }}
