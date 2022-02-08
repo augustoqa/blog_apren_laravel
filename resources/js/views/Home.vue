6f49abe09baf1f08b299f2e96f4f4593a12b372d
Cesar <chechaacual@gmail.com>
2022-02-07 19:03:08 -0600

    Paginacion con Vue Router

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,5 +1,22 @@
 <template>
-    <posts-list :posts="posts"></posts-list>
+    <div>
+        <posts-list :posts="posts"></posts-list>
+
+        <nav class="pagination" v-if="pagination.last_page > 1">
+            <ul class="list-unstyled container-flex space-center">
+                <li v-for="page in pagination.last_page">
+                    <router-link :class="getActiveClass(page)" :to="{
+                        name: 'home',
+                        query: {
+                            page: page
+                        }
+                    }">
+                        {{ page }}
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </template>
 
 <script>
@@ -10,17 +27,36 @@ export default {
     data () {
         return {
             posts: [],
+            pagination: {},
         }
     },
     mounted() {
-        axios.get('/api/posts')
+        axios.get(`/api/posts?page=${this.$route.query.page || 1}`)
             .then(response => {
                 this.posts = response.data.data;
+                this.pagination = response.data;
+                delete this.pagination.data;
             })
             .catch(err => {
                 console.log(err)
             })
+    },
+    methods: {
+        getActiveClass(page) {
+            return [
+                ! this.$route.query.page && page == 1 ? 'active' : ''
+            ]
+        }
     }
 }
 </script>
+
+<style lang="scss">
+.pagination {
+    a.active {
+        background-color: #1abc9c;
+        color: white;
+    }
+}
+</style>
 
